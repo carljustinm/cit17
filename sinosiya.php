18b7a25c8e6304509ea8cb9d536526e527c95b96
unknown <carlmasedman01@gmail.com>
2023-10-17 17:39:10 +0800

    added two new files

---
+++ sinosiya.php
@@ -0,0 +1,66 @@
+<html>
+    <title>Daleng Dale Lyrics</title>
+        <body>
+            <?php 
+            
+            echo "Daleng dale, daleng dale <br>
+            Grabe, daleng dale <br>
+            
+            Uy sino siya <br>
+            Ang lakas ng dating (3x) <br>
+            Ganyan <br>
+            Uy sino siya <br>
+            Pag nakitang gumiling <br>
+            Talagang manggigigil ka <br>
+            <br>
+            Ga-raa-ba-e <br>
+            Daleng dale mo <br>
+            Galaw na gusto ko <br>
+            Sige igiling igiling wag hihinto <br>
+            Daleng dale mo <br>
+            Sayaw na gusto ko <br>
+            Baby igiling igiling wag hihinto <br>
+            <br>
+            Grabe, daleng dale haypa <br>
+            Daleng dale haypa <br>
+            Daleng dale grabe <br>
+            Daleng dale haypa <br>
+            <br>
+            Uy sino siya <br>
+            Ang bangis niyang gumiling (3x) giling <br>
+            Uy sino siya <br>
+            Di na kayang pigilin <br>
+            Talangang manggigigil ka Ga-raa-ba-e <br>
+            Daleng dale mo <br>
+            Galaw na gusto ko <br>
+            Sige igiling igiling wag hihinto <br>
+            Daleng dale mo <br>
+            Sayaw na gusto ko <br>
+            Baby igiling igiling wag hihinto <br>
+            <br>
+            Grabe, daleng dale haypa <br>
+            Daleng dale haypa <br>
+            Daleng dale grabe <br>
+            Daleng dale haypa <br>
+            <br>
+            Uy sige lang <br>
+            Pababa (5x) <br>
+            Uy sige lang <br>
+            Pataas (6x) <br>
+            <br>
+            Galaw na gusto ko <br>
+            Sige igiling igiling wag hihinto <br>
+            Daleng dale mo <br>
+            Sayaw na gusto ko <br>
+            Baby igiling igiling wag hihinto <br>
+            <br>
+            Grabe, daleng dale haypa <br>
+            Daleng dale haypa <br>
+            Daleng dale grabe <br>
+            Daleng dale haypa <br>
+            <br>
+            (Repeat 4x)";
+            
+            ?>
+        </body>
+</html>
